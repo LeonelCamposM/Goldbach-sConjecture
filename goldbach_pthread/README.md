832832c5f3e34c1075867e9f30bb690026ba06c9
LeonelCamposM <leonelscamposm@gmail.com>
2022-01-20 14:31:35 -0600

    Copy last proyect, create Design for goldbach pthread

---
+++ goldbach_pthread/README.md
@@ -0,0 +1,23 @@
+# Goldbach serial
+
+This is the base program, it has an acceptable duration for small test cases, however it requires considerable time to obtain the solution for large test cases.
+
+## User manual 
+### How to make a manual test
+1) Compile the program: make
+![makeImg](img/Make.png)
+
+2) Run the program: ./bin/goldbach_serial
+![runImg](img/Execute.png)
+
+3) Write a number and press enter
+![writeImg](img/Write.png)
+
+### How to make a automatic test
+The script will execute all the test cases and report the time it took to execute each one.
+
+1) Run the script: ./check.sh
+![checkImg](img/Check.png)
+
+## Results
+Time calculating file input023: 792.41 seconds (1 thread)
---
+++ goldbach_pthread/src/dynamic_array/dynamic_array.c
@@ -0,0 +1,60 @@
+// Copyright 2021 Jeisson Hidalgo-Cespedes <jeisson.hidalgo@ucr.ac.cr> CC-BY-4
+
+#include <assert.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+
+#include "dynamic_array.h"
+
+int array_init(dynamic_array_t* array) {
+  assert(array);
+  array->capacity = 0;
+  array->count = 0;
+  array->elements = NULL;
+  return EXIT_SUCCESS;
+}
+
+void array_destroy(dynamic_array_t* array) {
+    assert(array);
+    array->capacity = 0;
+    array->count = 0;
+    free(array->elements);
+}
+
+/**
+ * @brief Increases the capacity of the dynamic array
+ * @param array A pointer to the array
+ * @return An integer, indicating if there was an error
+ */
+int array_increase_capacity(dynamic_array_t* array) {
+  size_t new_capacity = 10 *(array->capacity ? array->capacity : 1);
+  int64_t* new_elements = (int64_t*)
+  realloc(array->elements, new_capacity * sizeof(int64_t));
+  if (new_elements) {
+    array->capacity = new_capacity;
+    array->elements = new_elements;
+    return EXIT_SUCCESS;
+  } else {
+    return EXIT_FAILURE;
+  }
+}
+
+int array_append(dynamic_array_t* array, int64_t element) {
+  assert(array);
+  if (array->count == array->capacity) {
+    if (array_increase_capacity(array) != EXIT_SUCCESS) {
+      return EXIT_FAILURE;
+    }
+  }
+  array->elements[array->count++] = element;
+  return EXIT_SUCCESS;
+}
+
+void array_print(dynamic_array_t* array){
+  for (size_t i = 0; i < array->count; i++)
+  {
+    printf("%" PRIi64 " + ", array->elements[i]);
+  }
+}
