cbb6879463a8c5e35d7837b51b526bb7c188f49a
LeonelCamposM <leonelscamposm@gmail.com>
2022-02-04 12:49:42 -0600

    design port distribution, create client, server script

--- goldbach_server/wsl2_ports_conf.ps1
+++ goldbach_server/wsl2_ports_conf.ps1
@@ -1,11 +1,19 @@
-# Number of worker pc's
-$CONSUMERS_COUNT = 3
+# # Server pc conf
+# $CLIENT_COUNT = 3
+# $WSL_IP = "172.31.174.166"
+# $FIRST_PORT = 5000
+
+# for($i=0; $i -lt ($CLIENT_COUNT+2); $i++){
+#     $new_port = $FIRST_PORT+$i
+#     netsh interface portproxy add v4tov4 listenport=$new_port listenaddress=0.0.0.0 connectport=$new_port connectaddress=$WSL_IP
+# }
+
+# netsh interface portproxy show all
+
+# Client pc conf
 $WSL_IP = "172.31.174.166"
 $FIRST_PORT = 5000
 
-for($i=0; $i -lt $CONSUMERS_COUNT; $i++){
-    $new_port = $FIRST_PORT+$i
-    netsh interface portproxy add v4tov4 listenport=$new_port listenaddress=0.0.0.0 connectport=$new_port connectaddress=$WSL_IP
-}
+netsh interface portproxy add v4tov4 listenport=$FIRST_PORT listenaddress=0.0.0.0 connectport=$FIRST_PORT connectaddress=$WSL_IP
 
 netsh interface portproxy show all
--- goldbach_server/wsl2_ports_reset.ps1
+++ goldbach_server/wsl2_ports_reset.ps1
@@ -1,10 +1,17 @@
-# Number of worker pc's
-$CONSUMERS_COUNT = 3
+# # Server pc reset
+# $CLIENT_COUNT = 3
+# $FIRST_PORT = 5000
+
+# for($i=0; $i -lt $CLIENT_COUNT+2; $i++){
+#     $new_port = $FIRST_PORT+$i
+#     netsh interface portproxy delete v4tov4 listenport=$new_port listenaddress=0.0.0.0
+# }
+
+# netsh interface portproxy show all
+
+# Client pc reset
 $FIRST_PORT = 5000
 
-for($i=0; $i -lt $CONSUMERS_COUNT; $i++){
-    $new_port = $FIRST_PORT+$i
-    netsh interface portproxy delete v4tov4 listenport=$new_port listenaddress=0.0.0.0
-}
+netsh interface portproxy delete v4tov4 listenport=$FIRST_PORT listenaddress=0.0.0.0
 
 netsh interface portproxy show all
